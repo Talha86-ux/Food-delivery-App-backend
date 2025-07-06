172023647cfd6552d60cca904ce57b893ed12e30
Talha86-ux <rjtalha555@gmail.com>
2025-07-06 20:44:09 +0500

    Implement jwt auth with refresh token functionality

--- schemas.py
+++ schemas.py
@@ -2,25 +2,26 @@ from pydantic import BaseModel
 from typing import Optional
 
 class SignUp(BaseModel):
-  id: Optional[int]
-  username: str
-  email: str
-  password: str
-  is_staff: Optional[bool] = bool
-  is_active: Optional[bool] = bool
+    id: Optional[int]
+    username: str
+    email: str
+    password: str
+    is_staff: Optional[bool] = False
+    is_active: Optional[bool] = True
 
-  class Config:
-    orm_mode = True
-    schema_extra = {
-      "example": {
-        "username": "string",
-        "email": "string",
-        "password": "string",
-        "is_staff": True,
-        "is_active": True
-      }
-    }
+    class Config:
+        from_attributes = True
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "username": "string",
+                "email": "string",
+                "password": "string",
+                "is_staff": True,
+                "is_active": True
+            }
+        }
 
 class Login(BaseModel):
-  username: str
-  password: str
+    username: str
+    password: str
