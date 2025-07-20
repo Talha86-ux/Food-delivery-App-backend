5ccff6752adeb3bcbe9de2ba0b27db16355d6ffb
Talha86-ux <rjtalha555@gmail.com>
2025-07-21 01:34:48 +0500

    Setup Docker container for backend

--- database.py
+++ database.py
@@ -1,9 +1,21 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/pizza_delivery', echo=True)
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-Base=declarative_base()
+# Create async engine
+engine = create_async_engine(DATABASE_URL, echo=True)
 
-Session=sessionmaker()
+# Sessionmaker for AsyncSession
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+# Declarative base
+Base = declarative_base()
